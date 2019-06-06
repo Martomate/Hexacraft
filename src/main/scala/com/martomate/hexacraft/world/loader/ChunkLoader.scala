ee0fde97ff4f883d537d3442ba4b5665a7b5dc48
Martomate <martinnn97@hotmail.com>
2019-06-06 16:13:24 +0200

    Removed old chunk loader

--- src/main/scala/com/martomate/hexacraft/world/loader/ChunkLoader.scala
+++ src/main/scala/com/martomate/hexacraft/world/loader/ChunkLoader.scala
@@ -4,11 +4,6 @@ import com.martomate.hexacraft.world.chunk.IChunk
 import com.martomate.hexacraft.world.column.WorldListenerForTheLoader
 import com.martomate.hexacraft.world.coord.integer.ChunkRelWorld
 
-object ChunkLoader {
-  val chunksLoadedPerTick = 1
-  val ticksBetweenColumnLoading = 5
-}
-
 trait ChunkLoader extends WorldListenerForTheLoader {
   def tick(): Unit
   def chunksToAdd(): Iterable[IChunk]
