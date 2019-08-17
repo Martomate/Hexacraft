b744f8ceea76d2317095afa737e0c48b5135ca7a
Martin Jakobsson <martin.jakobsson@weiq.tech>
2019-08-17 23:47:46 +0200

    Fixed fullscreen issue Fixed crash if game is run from non-ascii path

--- src/main/scala/com/martomate/hexacraft/main/MainWindow.scala
+++ src/main/scala/com/martomate/hexacraft/main/MainWindow.scala
@@ -29,6 +29,7 @@ class MainWindow extends GameWindowExtended {
   private val window: Long = initWindow()
   private var fullscreen = false
   private var vsync = false
+  private var skipMouseMovedUpdate = false
 
   private val _mousePos = new Vector2d()
   private val _mouseMoved = new Vector2d()
@@ -55,13 +56,7 @@ class MainWindow extends GameWindowExtended {
   def resetMousePos(): Unit = {
     glfwGetCursorPos(window, doublePtrX, doublePtrY)
     _mousePos.set(doublePtrX.get(0), _windowSize.y - doublePtrY.get(0))
-  }
-
-  def moveMouse(pos: (Float, Float)): Unit = {
-    //val dx = _mousePos.x - prevWindowPos.x
-    //val dy = _mousePos.y - prevWindowPos.y
-    _mousePos.x = pos._1 * _windowSize.x
-    _mousePos.y = pos._2 * _windowSize.y
+    skipMouseMovedUpdate = true
   }
 
   private def loop(): Unit = {
@@ -143,7 +138,13 @@ class MainWindow extends GameWindowExtended {
     val oldMouseX = _mousePos.x
     val oldMouseY = _mousePos.y
     _mousePos.set(doublePtrX.get(0), _windowSize.y - doublePtrY.get(0))
-    _mouseMoved.set(_mousePos.x - oldMouseX, _mousePos.y - oldMouseY)
+
+    if (skipMouseMovedUpdate) {
+      _mouseMoved.set(0, 0)
+      skipMouseMovedUpdate = false
+    } else {
+      _mouseMoved.set(_mousePos.x - oldMouseX, _mousePos.y - oldMouseY)
+    }
 
     scenes.foreach(_.tick()) // TODO: should maybe be reversed
   }
@@ -197,27 +198,13 @@ class MainWindow extends GameWindowExtended {
       glfwGetWindowPos(window, intPtrX, intPtrY)
       prevWindowPos.set(intPtrX.get(0), intPtrY.get(0))
 
-
-      _mousePos.add(prevWindowPos.x, mode.height - prevWindowSize.y - prevWindowPos.y)
-
-      val cursorHidden = glfwGetInputMode(window, GLFW_CURSOR) == GLFW_CURSOR_DISABLED
-      if (cursorHidden) glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_HIDDEN)
       glfwSetWindowMonitor(window, monitor, 0, 0, mode.width(), mode.height(), mode.refreshRate())
-      resetMousePos()
-      if (cursorHidden) glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED)
-
-      fullscreen = true
     } else {
-      _mousePos.sub(prevWindowPos.x, mode.height - prevWindowSize.y - prevWindowPos.y)
-
-      val cursorHidden = glfwGetInputMode(window, GLFW_CURSOR) == GLFW_CURSOR_DISABLED
-      if (cursorHidden) glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_HIDDEN)
       glfwSetWindowMonitor(window, 0, prevWindowPos.x, prevWindowPos.y, prevWindowSize.x, prevWindowSize.y, GLFW_DONT_CARE)
-      resetMousePos()
-      if (cursorHidden) glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED)
-
-      fullscreen = false
     }
+
+    fullscreen = !fullscreen
+    skipMouseMovedUpdate = true
   }
 
   private def processChar(window: Long, character: Int): Unit = {
@@ -240,6 +227,7 @@ class MainWindow extends GameWindowExtended {
         scenes.foreach(_.windowResized(width, height))
       }
       _windowSize.set(width, height)
+      skipMouseMovedUpdate = true
     }
     Shader.foreach(_.setUniform2f("windowSize", _windowSize.x, _windowSize.y))
   }
