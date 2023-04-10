2de89bfb08c957d75b5f12938d848bfb65908560
Martin Jakobsson <martin@backtick.se>
2023-04-10 13:49:35 +0200

    Replaced ChunkLoadingEdgeListener with event tracking

--- src/main/scala/com/martomate/hexacraft/world/loader/ChunkLoadingEdgeListener.scala
+++
@@ -1,8 +0,0 @@
-package com.martomate.hexacraft.world.loader
-
-import com.martomate.hexacraft.world.coord.integer.ChunkRelWorld
-
-trait ChunkLoadingEdgeListener {
-  def onChunkOnEdge(chunk: ChunkRelWorld, onEdge: Boolean): Unit
-  def onChunkLoadable(chunk: ChunkRelWorld, loadable: Boolean): Unit
-}
