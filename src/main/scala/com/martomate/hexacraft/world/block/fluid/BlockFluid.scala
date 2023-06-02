9a1fbe0828638bb70f747264855e5300970cc6d6
Martin Jakobsson <martin@backtick.se>
2023-06-02 22:57:35 +0200

    Fixed small issue

--- src/main/scala/com/martomate/hexacraft/world/block/fluid/BlockFluid.scala
+++ src/main/scala/com/martomate/hexacraft/world/block/fluid/BlockFluid.scala
@@ -3,7 +3,7 @@ package com.martomate.hexacraft.world.block.fluid
 import com.martomate.hexacraft.world.block.{Block, BlockBehaviour, BlockLoader, Blocks}
 
 class BlockFluid(_id: Byte, _name: String, _displayName: String) extends Block(_id, _name, _displayName) {
-  override protected val behaviour: Option[BlockBehaviour] = Some(new BlockBehaviourFluid)
+  override val behaviour: Option[BlockBehaviour] = Some(new BlockBehaviourFluid)
 
   override def isTransparent(metadata: Byte, side: Int): Boolean = metadata != 0
 
