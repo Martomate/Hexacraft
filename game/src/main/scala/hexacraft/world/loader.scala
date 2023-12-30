7b3d1c4ec8fb3c14a20d92f2d173b3821ff5ef6e
Martin Jakobsson <martin@backtick.se>
2023-12-30 02:10:53 +0100

    Refactor: Moved network code to separate file

--- game/src/main/scala/hexacraft/world/loader.scala
+++ game/src/main/scala/hexacraft/world/loader.scala
@@ -8,7 +8,6 @@ import java.util.concurrent.TimeUnit
 import scala.collection.mutable
 import scala.concurrent.{Await, Future}
 import scala.concurrent.duration.Duration
-import scala.util.Try
 
 class ChunkLoader(
     chunkFactory: ChunkRelWorld => Chunk,
