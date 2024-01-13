45c694d4951eeeb90be5c7d5755b0e2faac97c08
Martin Jakobsson <martin@backtick.se>
2024-01-13 18:57:13 +0100

    Updated dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,10 +1,8 @@
 import sbt.*
 
-import scala.collection.immutable.Seq
-
 object Dependencies {
   object versions {
-    val lwjgl = "3.3.2"
+    val lwjgl = "3.3.3"
     val joml = "1.10.5"
     val zeromq = "0.5.4"
   }
@@ -37,9 +35,9 @@ object Dependencies {
   lazy val ZeroMQ = "org.zeromq" % "jeromq" % versions.zeromq
 
   lazy val MUnit = "org.scalameta" %% "munit" % "0.7.29" % "test"
-  lazy val Mockito = "org.scalatestplus" %% "mockito-4-11" % "3.2.16.0" % "test"
+  lazy val Mockito = "org.scalatestplus" %% "mockito-5-8" % "3.2.17.0" % "test"
   lazy val ArchUnit = Seq(
-    "com.tngtech.archunit" % "archunit" % "1.0.1" % "test",
-    "org.slf4j" % "slf4j-nop" % "2.0.5" % "test" // Needed for ArchUnit
+    "com.tngtech.archunit" % "archunit" % "1.2.1" % "test",
+    "org.slf4j" % "slf4j-nop" % "2.0.11" % "test" // Needed for ArchUnit
   )
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,4 +1,4 @@
-addSbtPlugin("org.scoverage" % "sbt-scoverage" % "2.0.4")
-addSbtPlugin("org.scoverage" % "sbt-coveralls" % "1.3.2")
-addSbtPlugin("org.scalameta" % "sbt-scalafmt" % "2.4.6")
+addSbtPlugin("org.scoverage" % "sbt-scoverage" % "2.0.9")
+addSbtPlugin("org.scoverage" % "sbt-coveralls" % "1.3.11")
+addSbtPlugin("org.scalameta" % "sbt-scalafmt" % "2.5.2")
 addSbtPlugin("org.xerial.sbt" % "sbt-pack" % "0.18")
