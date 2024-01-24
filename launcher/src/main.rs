7ce16f6050f632884dbad91c4bbabfed9fbf8306
Martin Jakobsson <martin@backtick.se>
2024-01-24 11:57:06 +0100

    Launcher remake: Fetch game version from API

--- launcher/src/main.rs
+++ launcher/src/main.rs
@@ -1,6 +1,13 @@
 #![allow(clippy::type_complexity)]
 
-use bevy::{prelude::*, window::RequestRedraw, winit::WinitSettings};
+use bevy::{
+    prelude::*,
+    tasks::{block_on, IoTaskPool, Task},
+    window::RequestRedraw,
+    winit::WinitSettings,
+};
+use futures_lite::future;
+use serde::Deserialize;
 
 fn main() {
     App::new()
@@ -14,16 +21,34 @@ fn main() {
             ..Default::default()
         }))
         .add_event::<Action>()
-        .insert_resource(State::default())
+        .insert_resource(MainState::default())
         // Only run the app when there is user input. This will significantly reduce CPU/GPU use.
         .insert_resource(WinitSettings::desktop_app())
         .add_systems(Startup, setup)
-        .add_systems(Update, (button_system, perform_actions))
+        .add_systems(
+            Update,
+            (
+                handle_http_tasks,
+                button_system,
+                perform_actions,
+                update_available_versions_list,
+            ),
+        )
         .run();
 }
 
+#[derive(Debug, PartialEq, Eq, Deserialize)]
+struct GameVersion {
+    id: String,
+    name: String,
+    release_date: String,
+    url: String,
+    file_to_run: String,
+}
+
 #[derive(Resource, Default)]
-struct State {
+struct MainState {
+    available_versions: Option<Vec<GameVersion>>,
     selected_version: Option<String>,
 }
 
@@ -38,9 +63,52 @@ enum Action {
 struct VersionSelector;
 
 #[derive(Component)]
+struct AvailableVersionsList;
+
+#[derive(Component)]
 struct VersionButton;
 
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>, state: Res<State>) {
+enum HttpTaskResponse {
+    FetchGameVersions(Vec<GameVersion>),
+}
+
+#[derive(Component)]
+struct HttpTask(Task<anyhow::Result<HttpTaskResponse>>);
+
+fn handle_http_tasks(
+    mut commands: Commands,
+    mut http_tasks: Query<(Entity, &mut HttpTask)>,
+    mut main_state: ResMut<MainState>,
+) {
+    for (entity, mut task) in &mut http_tasks {
+        if let Some(res) = block_on(future::poll_once(&mut task.0)) {
+            match res {
+                Ok(res) => match res {
+                    HttpTaskResponse::FetchGameVersions(versions) => {
+                        main_state.available_versions = Some(versions)
+                    }
+                },
+                Err(err) => println!("Failed to make http request: {}", err),
+            }
+            commands.entity(entity).despawn();
+        }
+    }
+}
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>, state: Res<MainState>) {
+    let task_pool = IoTaskPool::get();
+    let entity = commands.spawn_empty().id();
+    let task: Task<anyhow::Result<_>> = task_pool.spawn(async {
+        let url = "https://martomate.com/api/hexacraft/versions";
+
+        let response = ureq::get(url).call()?;
+        let body = response.into_string()?;
+
+        let versions = serde_json::from_str::<Vec<GameVersion>>(&body)?;
+        Ok(HttpTaskResponse::FetchGameVersions(versions))
+    });
+    commands.entity(entity).insert(HttpTask(task));
+
     commands.spawn(Camera2dBundle::default());
 
     commands.spawn(SpriteBundle {
@@ -135,7 +203,7 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>, state: Res<Stat
                         .insert(VersionSelector)
                         .with_children(|parent| {
                             parent
-                                .spawn((NodeBundle {
+                                .spawn(NodeBundle {
                                     style: Style {
                                         position_type: PositionType::Absolute,
                                         left: Val::Px(5.0),
@@ -146,37 +214,9 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>, state: Res<Stat
                                     },
                                     background_color: Color::rgba(0.15, 0.15, 0.15, 0.8).into(),
                                     ..default()
-                                },))
-                                .with_children(|parent| {
-                                    for i in 1..=12 {
-                                        parent
-                                            .spawn(ButtonBundle {
-                                                style: Style {
-                                                    height: Val::Px(24.0),
-                                                    width: Val::Px(100.0),
-                                                    justify_content: JustifyContent::FlexStart,
-                                                    margin: UiRect::axes(Val::Px(8.0), Val::Px(2.0)),
-                                                    ..default()
-                                                },
-                                                background_color: Color::rgba(
-                                                    0.15, 0.15, 0.15, 0.0,
-                                                )
-                                                .into(),
-                                                ..default()
-                                            })
-                                            .insert(Action::SelectVersion(format!("0.{}", i)))
-                                            .with_children(|parent| {
-                                                parent.spawn(TextBundle::from_section(
-                                                    format!("0.{}", i),
-                                                    TextStyle {
-                                                        font: asset_server.load("Verdana.ttf"),
-                                                        font_size: 20.0,
-                                                        ..default()
-                                                    },
-                                                ));
-                                            });
-                                    }
-                                });
+                                })
+                                .insert(AvailableVersionsList)
+                                .with_children(|_| {}); // make sure there is a Children component
                         });
 
                     parent
@@ -209,6 +249,56 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>, state: Res<Stat
         });
 }
 
+fn update_available_versions_list(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    q_list: Query<(Entity, &Children), With<AvailableVersionsList>>,
+    state: Res<MainState>,
+) {
+    if !state.is_changed() {
+        return;
+    }
+
+    if let Some(ref versions) = state.available_versions {
+        let mut items = Vec::new();
+
+        for v in versions.iter() {
+            let item = commands
+                .spawn(ButtonBundle {
+                    style: Style {
+                        height: Val::Px(24.0),
+                        width: Val::Px(100.0),
+                        justify_content: JustifyContent::FlexStart,
+                        margin: UiRect::axes(Val::Px(8.0), Val::Px(2.0)),
+                        ..default()
+                    },
+                    background_color: Color::rgba(0.15, 0.15, 0.15, 0.0).into(),
+                    ..default()
+                })
+                .insert(Action::SelectVersion(v.name.clone()))
+                .with_children(|parent| {
+                    parent.spawn(TextBundle::from_section(
+                        v.name.clone(),
+                        TextStyle {
+                            font: asset_server.load("Verdana.ttf"),
+                            font_size: 20.0,
+                            ..default()
+                        },
+                    ));
+                })
+                .id();
+
+            items.push(item);
+        }
+
+        let (list, old_children) = q_list.single();
+        commands.entity(list).replace_children(&items);
+        for ch in old_children {
+            commands.entity(*ch).despawn_recursive();
+        }
+    }
+}
+
 fn button_system(
     mut interaction_query: Query<(&Interaction, &Action), (Changed<Interaction>, With<Button>)>,
     mut redraw_event_writer: EventWriter<RequestRedraw>,
@@ -228,14 +318,14 @@ fn perform_actions(
     mut q_version_selector: Query<&mut Visibility, With<VersionSelector>>,
     q_version_button: Query<&Children, With<VersionButton>>,
     mut q_text: Query<&mut Text>,
-    mut state: ResMut<State>,
+    mut state: ResMut<MainState>,
 ) {
     for action in &mut action_event_reader.read() {
         match action {
             Action::Play => println!("Play pressed!"),
             Action::SelectVersion(version) => {
                 state.selected_version = Some(version.clone());
-                
+
                 let version_button_children = q_version_button.single();
                 let mut version_button_text = q_text.get_mut(version_button_children[0]).unwrap();
                 version_button_text.sections[0].value = format!("Version: {}", *version);
@@ -286,4 +376,33 @@ fn make_outlined_text(
     let main_bundle = TextBundle::from_section(text, style);
 
     (outline_bundles, main_bundle)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_game_version() -> serde_json::Result<()> {
+        let json_str = r#"{
+            "id": "0.1",
+            "name": "0.1",
+            "release_date": "2015-06-20T00:37:30+02:00",
+            "url": "https://a.com/b/c.zip",
+            "file_to_run": "Game.jar"
+        }"#;
+
+        assert_eq!(
+            serde_json::from_str::<GameVersion>(json_str)?,
+            GameVersion {
+                id: "0.1".into(),
+                name: "0.1".into(),
+                release_date: "2015-06-20T00:37:30+02:00".into(),
+                url: "https://a.com/b/c.zip".into(),
+                file_to_run: "Game.jar".into(),
+            }
+        );
+
+        Ok(())
+    }
 }
