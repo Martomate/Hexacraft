b076e92cdec57298d95b1f3a9b5972f766e2c277
Martin Jakobsson <martin@backtick.se>
2024-02-27 23:54:02 +0100

    Refactor: Improved the RenderAspectHandler

--- game/src/main/scala/hexacraft/world/render/RenderAspectHandler.scala
+++ game/src/main/scala/hexacraft/world/render/RenderAspectHandler.scala
@@ -18,91 +18,61 @@ class RenderAspectHandler(bufferHandler: BufferHandler[_]) {
       chunksToClear: Seq[ChunkRelWorld],
       chunksToUpdate: Seq[(ChunkRelWorld, ByteBuffer)]
   ): Unit = {
-    memorySegments.synchronized {
-      // TODO: perform the operations below in batches or all at once
-      for coords <- chunksToClear do {
-        removeData(coords, memorySegments.segments(coords))
-      }
-      for (coords, content) <- chunksToUpdate do {
-        if memorySegments.hasMapping(coords) then {
-          updateChunk(coords, content)
-        } else {
-          addNewChunk(coords, content)
-        }
-      }
-    }
-  }
+    val tempCoords = ChunkRelWorld(-1L) // TODO: make non-temp solution
 
-  def unload(): Unit = {
-    bufferHandler.unload()
-  }
-
-  private def appendData(coords: ChunkRelWorld, data: ByteBuffer): Unit = {
-    val numBytes = data.remaining()
-    if numBytes > 0 then {
-      val seg = memorySegments.push(coords, numBytes)
-      bufferHandler.set(seg, data)
-    }
-  }
-
-  private def addNewChunk(coords: ChunkRelWorld, data: ByteBuffer): Unit = {
-    appendData(coords, data)
-  }
-
-  private def updateChunk(coords: ChunkRelWorld, data: ByteBuffer): Unit = {
-    val oldLen = memorySegments.totalLengthForChunk(coords)
-    val newLen = data.remaining()
-
-    if oldLen == newLen then { // The new data fits perfectly in the allocated space
-      // overwrite the old data with the new data
+    // Step 1: move old data to temporary label
+    for coords <- chunksToClear do {
       for s <- memorySegments.segments(coords) do {
-        bufferHandler.set(s, data)
+        memorySegments.relabel(coords, tempCoords, s)
       }
-    } else if oldLen < newLen then { // The new data takes more space than the old data
-      // overwrite the old data with as much of the new data as possible
-      for s <- memorySegments.segments(coords) do {
-        bufferHandler.set(s, data)
-      }
-      // append the rest of the data at the end of the buffer
-      appendData(coords, data)
-    } else { // The new data takes less space than the old data
-      val (before, after) = memorySegments.segments(coords).cut(newLen)
-
-      // overwrite the old data with the new data
-      for s <- before do {
-        bufferHandler.set(s, data)
-      }
-
-      // remove the space used by the old data but not needed by the new data (by moving data from the end of buffer)
-      removeData(coords, after)
     }
-  }
+    for (coords, _) <- chunksToUpdate do {
+      for s <- memorySegments.segments(coords) do {
+        memorySegments.relabel(coords, tempCoords, s)
+      }
+    }
 
-  private def removeData(coords: ChunkRelWorld, segments: SegmentSet): Unit = {
+    // Step 2: fill data into the space of the old data and push the rest at the end
+    for (coords, data) <- chunksToUpdate do {
+      val (dest, _) = memorySegments.segments(tempCoords).cut(data.remaining())
+      for s <- dest do {
+        memorySegments.relabel(tempCoords, coords, s)
+        bufferHandler.set(s, data)
+      }
+      val remaining = data.remaining()
+      if remaining > 0 then {
+        val s = memorySegments.push(coords, remaining)
+        bufferHandler.set(s, data)
+      }
+    }
+
+    // Step 3: fill gap if needed by moving data from the end
+    val segments = memorySegments.segments(tempCoords)
     while segments.totalLength > 0 do {
-      val lastRem = segments.lastSegment()
       val (lastChunk, lastSeg) = memorySegments.lastSegment.get
-      if coords == lastChunk then {
-        // remove it
-        memorySegments.pop(coords, lastRem)
-        // TODO: the line above has crashed multiple times, not sure why
-        //  EDIT: just added synchronization blocks further up, let's hope it solves it
 
+      if tempCoords == lastChunk then {
+        val lastRem = segments.lastSegment()
+        memorySegments.pop(tempCoords, lastRem)
         segments.remove(lastRem)
       } else {
-        // move data
         val firstRem = segments.firstSegment()
         val len = math.min(firstRem.length, lastSeg.length)
 
         val dataSegment = Segment(lastSeg.start + lastSeg.length - len, len)
         val holeSegment = Segment(firstRem.start, len)
 
+        // TODO: merge segments from different chunks at the end of the buffers so more can be moved at once
         bufferHandler.copy(dataSegment.start, holeSegment.start, len)
 
         memorySegments.pop(lastChunk, dataSegment)
-        memorySegments.relabel(coords, lastChunk, holeSegment)
+        memorySegments.relabel(tempCoords, lastChunk, holeSegment)
         segments.remove(holeSegment)
       }
     }
+  }
+
+  def unload(): Unit = {
+    bufferHandler.unload()
   }
 }
