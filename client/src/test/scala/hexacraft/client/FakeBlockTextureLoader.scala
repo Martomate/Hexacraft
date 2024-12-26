1707b3b64a37f5fe7bd5d9a7feece1976f18dad4
Martin Jakobsson <martin@backtick.se>
2024-12-26 01:51:39 +0100

    Fixed broken test

--- client/src/test/scala/hexacraft/client/FakeBlockTextureLoader.scala
+++ client/src/test/scala/hexacraft/client/FakeBlockTextureLoader.scala
@@ -1,7 +1,15 @@
 package hexacraft.client
 
 import hexacraft.renderer.PixelArray
+import hexacraft.util.Result
+
+import java.io.FileNotFoundException
 
 class FakeBlockTextureLoader extends BlockTextureLoader:
-  override def load(squareTextureNames: Seq[String], triTextureNames: Seq[String]): BlockTextureLoader.LoadedImages =
-    BlockTextureLoader.LoadedImages(Seq(PixelArray(Array.fill(32 * 32)(0), false)), Map.empty.withDefault(_ => 0))
+  override def load(
+      squareTextureNames: Seq[String],
+      triTextureNames: Seq[String]
+  ): Result[BlockTextureLoader.LoadedImages, FileNotFoundException] =
+    Result.Ok(
+      BlockTextureLoader.LoadedImages(Seq(PixelArray(Array.fill(32 * 32)(0), false)), Map.empty.withDefault(_ => 0))
+    )
