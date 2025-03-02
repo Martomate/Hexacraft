b5cfaedb34247d284ee1001824164e5b80a31969
Martin Jakobsson <martin@backtick.se>
2025-03-02 21:32:45 +0100

    Made Rust JNI more ergonomic

--- native/src/native/src/lib.rs
+++ native/src/native/src/lib.rs
@@ -1,12 +1,10 @@
 use jni::objects::JObject;
 use jni::sys::jstring;
 use jni::JNIEnv;
+use jni_fn::jni_fn;
 
-#[no_mangle]
-pub extern "system" fn Java_hexacraft_rs_RustLib_00024_hello<'local>(
-    env: JNIEnv<'local>,
-    _object: JObject<'local>,
-) -> jstring {
+#[jni_fn("hexacraft.rs.RustLib$")]
+pub fn hello<'local>(env: JNIEnv<'local>, _object: JObject<'local>) -> jstring {
     let s = env.new_string("Hello from Rust!").unwrap();
     s.into_raw()
 }
