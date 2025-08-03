91bb4155a1b19f371165260c9aa7a920596a0901
Martin Jakobsson <martin@backtick.se>
2025-08-03 19:18:20 +0200

    Launcher: Separate CI workflows for launcher and game

--- launcher/build.rs
+++ launcher/build.rs
@@ -3,6 +3,6 @@ fn main() {
 
     if cfg!(windows) {
         println!("cargo::rerun-if-changed=packaging/windows/resources.rc");
-        embed_resource::compile("packaging/windows/resources.rc", embed_resource::NONE).manifest_optional().unwrap();
+        embed_resource::compile("packaging/windows/resources.rc", embed_resource::NONE).manifest_required().unwrap();
     }
 }
