6f93bc940e41253ab5acf8e316a3bc1b8223e988
Martin Jakobsson <martin@backtick.se>
2025-08-04 13:24:57 +0200

    Launcher: Move ui action code into separate file

--- launcher/src/ui/mod.rs
+++ launcher/src/ui/mod.rs
@@ -1,5 +1,6 @@
 #![allow(clippy::type_complexity)]
 
+mod actions;
 mod elements;
 
 use crate::core::GameVersion;
@@ -12,7 +13,7 @@ use bevy::{
 };
 use std::{
     ops::Deref,
-    sync::{Arc, Mutex, mpsc},
+    sync::{Arc, Mutex},
 };
 
 pub trait UiHandler: Send + Sync + 'static {
@@ -91,7 +92,7 @@ pub fn run<H: UiHandler>(handler: H) {
             (
                 handle_http_tasks::<H>,
                 button_system,
-                perform_actions::<H>,
+                actions::perform_actions::<H>,
                 update_available_versions_list,
                 update_download_progress,
             ),
@@ -174,13 +175,11 @@ fn setup<H: UiHandler>(
     handler: Res<UiHandlerRes<H>>,
 ) {
     let task_pool = AsyncComputeTaskPool::get();
-    let entity = commands.spawn_empty().id();
 
-    let res = handler.fetch_versions_list();
-
-    let task =
-        HttpTask(task_pool.spawn(async { Ok(HttpTaskResponse::FetchGameVersions(res.await?)) }));
-    commands.entity(entity).insert(task);
+    commands.spawn(HttpTask(task_pool.spawn({
+        let res = handler.fetch_versions_list();
+        async { Ok(HttpTaskResponse::FetchGameVersions(res.await?)) }
+    })));
 
     commands.spawn(Camera2dBundle::default());
 
@@ -285,93 +284,4 @@ fn start_game<H: UiHandler>(handler: &H, game_version: &GameVersion) -> ! {
     let mut cmd = handler.start_game(game_version).unwrap();
     let _ = cmd.spawn().unwrap();
     std::process::exit(0);
-}
-
-fn perform_actions<H: UiHandler>(
-    mut commands: Commands,
-    mut action_event_reader: EventReader<Action>,
-    mut q_version_selector: Query<&mut Visibility, With<VersionSelector>>,
-    q_version_button: Query<&Children, With<VersionButton>>,
-    mut q_text: Query<&mut Text>,
-    mut state: ResMut<MainState>,
-    handler: Res<UiHandlerRes<H>>,
-) {
-    for action in &mut action_event_reader.read() {
-        match action {
-            Action::Play => {
-                if state.is_downloading {
-                } else if let Some(ref versions) = state.available_versions {
-                    let game_version = match state.selected_version {
-                        Some(ref s) => versions.iter().find(|&v| v.name == *s),
-                        None => versions.last(),
-                    };
-                    if let Some(game_version) = game_version {
-                        if handler.is_downloaded(game_version) {
-                            start_game(&handler.0, game_version);
-                        } else {
-                            let game_version = game_version.clone();
-
-                            let task_pool = AsyncComputeTaskPool::get();
-                            let entity = commands.spawn_empty().id();
-
-                            state.is_downloading = true;
-
-                            let download_progress = Arc::new(Mutex::new(0.0));
-                            let (progress_tx, progress_rx) = mpsc::channel();
-
-                            task_pool
-                                .spawn({
-                                    let download_progress = download_progress.clone();
-                                    async move {
-                                        while let Ok((bytes, total)) = progress_rx.recv() {
-                                            let progress = if total != 0 {
-                                                bytes as f32 / total as f32
-                                            } else {
-                                                0.0
-                                            };
-                                            *download_progress.lock().unwrap() = progress;
-                                        }
-                                    }
-                                })
-                                .detach();
-
-                            let task = HttpTask(task_pool.spawn({
-                                let download_task = handler.download(&game_version, progress_tx);
-                                async move {
-                                    let buffer = download_task.await?;
-                                    Ok(HttpTaskResponse::FetchGame(game_version, buffer))
-                                }
-                            }));
-                            commands
-                                .entity(entity)
-                                .insert(task)
-                                .insert(DownloadProgress(download_progress));
-                        }
-                    }
-                } else {
-                    println!(
-                        "Version list is not available yet. Maybe you don't have an internet connection?"
-                    )
-                }
-            }
-            Action::SelectVersion(version) => {
-                state.selected_version = Some(version.name.clone());
-
-                let version_button_children = q_version_button.single();
-                let mut version_button_text = q_text.get_mut(version_button_children[0]).unwrap();
-                version_button_text.sections[0].value = format!("Version: {}", version.name);
-
-                let mut version_selector = q_version_selector.single_mut();
-                *version_selector = Visibility::Hidden;
-            }
-            Action::ShowVersionSelector => {
-                let mut version_selector = q_version_selector.single_mut();
-                *version_selector = Visibility::Visible;
-            }
-            Action::HideVersionSelector => {
-                let mut version_selector = q_version_selector.single_mut();
-                *version_selector = Visibility::Hidden;
-            }
-        };
-    }
 }
