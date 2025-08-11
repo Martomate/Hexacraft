0ee8876babf15ed8dfd08ebaa92e89cb3e336c1b
Martin Jakobsson <martin@backtick.se>
2025-08-11 23:02:16 +0200

    Deduped some code

--- client/src/main/scala/hexacraft/renderer/PixelArray.scala
+++ client/src/main/scala/hexacraft/renderer/PixelArray.scala
@@ -1,3 +1,20 @@
 package hexacraft.renderer
 
-case class PixelArray(pixels: Array[Int], isTriImage: Boolean)
+import org.joml.Vector3f
+
+case class PixelArray(pixels: Array[Int], isTriImage: Boolean) {
+  def averageColor: Vector3f = {
+    var r = 0L
+    var g = 0L
+    var b = 0L
+
+    for pix <- this.pixels do {
+      r += (pix >> 16) & 0xff
+      g += (pix >> 8) & 0xff
+      b += (pix >> 0) & 0xff
+    }
+
+    val c = this.pixels.length * 255
+    Vector3f(r.toFloat / c, g.toFloat / c, b.toFloat / c)
+  }
+}
