f61cebbaa1259391873bea756c74c7d4812ab93a
Martin Jakobsson <martin@backtick.se>
2026-07-29 08:29:14 +0200

    handle player movement

--- native/src/native/crates/core/src/server/input.rs
+++ native/src/native/crates/core/src/server/input.rs
@@ -2,10 +2,105 @@ use std::f64::consts::PI;
 
 use glam::{DVec3, Vec2};
 
-use crate::server::world::{Player, };
+use crate::server::world::Player;
 
 pub fn update_player(player: &mut Player, mouse_movement: Vec2, pressed_keys: &[&str]) {
+    let max_speed = determine_max_speed(pressed_keys);
+
+    update_velocity(
+        &mut player.velocity,
+        &player.rotation,
+        player.flying,
+        false,
+        max_speed,
+        pressed_keys,
+    );
     update_rotation(&mut player.rotation, mouse_movement, pressed_keys, 0.05);
+
+    let vel_len = player.velocity.x.hypot(player.velocity.z);
+    if vel_len > max_speed {
+      player.velocity.x *= max_speed / vel_len;
+      player.velocity.z *= max_speed / vel_len;
+    }
+
+    if player.flying {
+      player.position += player.velocity / 60.0;
+
+      player.velocity.x *= 0.8;
+      player.velocity.z *= 0.8;
+    } else {
+        let friction_factor = 0.8;
+        player.velocity.x *= friction_factor;
+        player.velocity.z *= friction_factor;
+        
+        player.position += player.velocity / 60.0;
+    }
+}
+
+fn determine_max_speed(pressed_keys: &[&str]) -> f64 {
+    if pressed_keys.contains(&"MoveSlowly") {
+        0.075
+    } else if pressed_keys.contains(&"MoveFast") {
+        12.0
+    } else if pressed_keys.contains(&"MoveSuperFast") {
+        120.0
+    } else {
+        4.3
+    }
+}
+
+fn update_velocity(
+    velocity: &mut DVec3,
+    rotation: &DVec3,
+    flying: bool,
+    in_fluid: bool,
+    max_speed: f64,
+    pressed_keys: &[&str],
+) {
+    if flying {
+        velocity.y = 0.0;
+    }
+
+    let cos_move = rotation.y.cos() * max_speed * 0.5;
+    let sin_move = rotation.y.sin() * max_speed * 0.5;
+
+    if pressed_keys.contains(&"MoveForward") {
+        velocity.z -= cos_move;
+        velocity.x += sin_move;
+    }
+
+    if pressed_keys.contains(&"MoveBackward") {
+        velocity.z += cos_move;
+        velocity.x -= sin_move;
+    }
+
+    if pressed_keys.contains(&"MoveRight") {
+        velocity.x += cos_move;
+        velocity.z += sin_move;
+    }
+
+    if pressed_keys.contains(&"MoveLeft") {
+        velocity.x -= cos_move;
+        velocity.z -= sin_move;
+    }
+
+    if pressed_keys.contains(&"Jump") {
+        if flying {
+            velocity.y = max_speed;
+        } else if velocity.y == 0.0 {
+            velocity.y = 5.0;
+        } else if in_fluid {
+            velocity.y += max_speed * 0.04;
+        }
+    }
+
+    if pressed_keys.contains(&"Sneak") {
+        if flying {
+            velocity.y = -max_speed;
+        } else if in_fluid {
+            velocity.y -= max_speed * 0.04;
+        }
+    }
 }
 
 fn update_rotation(
